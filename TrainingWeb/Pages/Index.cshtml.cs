006f9e70f315a45b545aad2cc554f29deb7102f7
Fernando Mercadal <fermercadal@outlook.com>
2020-04-21 10:43:15 -0300

    Adds json data, model, service and renders products title on index razor page

--- TrainingWeb/Pages/Index.cshtml.cs
+++ TrainingWeb/Pages/Index.cshtml.cs
@@ -5,21 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TrainingWeb.Models;
+using TrainingWeb.Services;
 
 namespace TrainingWeb.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
         }
 
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
